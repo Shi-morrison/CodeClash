20d09dad3e022ce35dc88517136207c2b0516545
iluvshi <shian.morrison@gmail.com>
2023-11-04 12:27:07 -0400

    bracket fix and dotenv install

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import m from "mithril";
 import { io } from "socket.io-client";
 
-import  Login from "./pages/Login";
+import Login from "./pages/Login";
 
 function App() {
     const socket = io("http://localhost:3000");
@@ -10,12 +10,13 @@ function App() {
     return {
         view: () => (
             <div>
-                <Login/>
+                <Login />
             </div>
         ),
     };
 
 
-m.route(document.body, "/", {
-  "/": App,
-});
+    m.route(document.body, "/", {
+        "/": App,
+    });
+}
