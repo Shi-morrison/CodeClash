acd832832551c15f11c946baa26c46e687b5188a
Bombachicky <kristianjeanmichel@gmail.com>
2023-11-11 23:07:23 -0500

    fixed leadeboard formatting

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,10 +3,7 @@ import axios from 'axios';
 import m from 'mithril';
 import FileUpload from '../components/FileUpload'
 
-
-
-function Navbar() {
-
+function Navbar(){
     let userData = {
         username: '...Loading',
         rank: '..Loading',
@@ -17,62 +14,50 @@ function Navbar() {
     axios.get('http://localhost:44251/api/current_user', {
         withCredentials: true // Important for including session cookies
     })
-        .then(response => {
-            const data = response.data;
-            console.log("data" + data.user);
-            if (data.user) {
-                userData = {
-                    username: data.user.username,
-                    rank: data.user.rank,
-                    profilePicFilename: data.user.profilePicture,
-                    ID: data.user._id
-                };
+    .then(response => {
+        const data = response.data;
+        console.log("data" + data.user);
+        if (data.user) {
+            userData = {
+                username: data.user.username,
+                rank: data.user.rank,
+                profilePicFilename: data.user.profilePicture,
+                ID: data.user._id
+            };
 
-                m.redraw();
-                // Update state or context with user data
-            } else {
-                console.error('User not authenticated');
-                // Handle not authenticated case
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-
-    // const profilePicUrl = userData.profilePicFilename
-    //     ? `${profile}`
-    //     : 'https://tetr.io/res/avatar.png';
+            m.redraw();
+            // Update state or context with user data
+        } else {
+            console.error('User not authenticated');
+            // Handle not authenticated case
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+    
     return {
         view: () => (
             <div className="invisible md:visible text-white flex justify-between px-16 navbar">
                 <div className="flex flex-row items-center">
                     <div className="cursor-pointer">
-                        <BackArrow />
+                        <BackArrow/>
                     </div>
                     <div className="text-[32px]">Leaderboard</div>
                 </div>
-
-                <div className="flex flex-row profile mb-2 mt-1">
-                    <div className="flex flex-col profile">
-                        <div>{userData.username}</div>
-                        <div className="rank">{userData.rank}</div>
-
                 <div className="flex flex-row profileNavbar mb-2 mt-1">
                     <div className="flex flex-col profileNavbar">
                         <div className="flex justify-center">
                             <div>Profile</div>  
                         </div>
                         <div className="rank">Rank</div>
-
                     </div>
                     <div className="flex items-center">
-                        <img className="img rounded" src={userData.profilePicFilename} />
-                        <FileUpload userId={userData.ID} />
+                        <img className="img rounded" src="https://tetr.io/res/avatar.png"/>
                     </div>
                 </div>
             </div>
         )
     }
 }
-
 export default Navbar;
